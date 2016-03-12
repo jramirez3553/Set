a895a8e5053f6cac860dfa76c8ab97d93a53f2ae
jaime <ramirezjaime0@gmail.com>
2016-03-11 22:19:52 -0600

    This is the second committ.  I add the equal method.

--- Set.java
+++ Set.java
@@ -26,21 +26,50 @@ public class Set {
 
     }
 
+    private int indexOf(Object element){
+        for (int i = 0; i < size; i++) {
+            if (aSet[i].equals(element))
+                return i;
+        }
+
+                return -1;
+    }
+
     public void remove(Object element){
-        for (int i = 0; i < size ; i++)
-            if (aSet[i].equals(element)) {
-                aSet[i] = aSet[size - 1];
-                size = size - 1;
-                size--;
-            }
+           int i = indexOf(element);
+            if(i == -1)
+                return;
+           aSet[i] = aSet[size - 1];
+           size--;
+
     }
 
     public boolean contains(Object element) {
+       if(indexOf(element) == -1)
+           return false;
+        else return true;
+    }
+
+    public boolean equals(Object element){
+        if(element == null)
+            return false;
+        if(getClass() != element.getClass())
+            return false;
+        Set anotherSet = (Set)element;
+        if (size() != anotherSet.size())
+            return false;
+
+        int count =0;
         for (int i = 0; i < size; i++) {
-            if (aSet[i].equals(element))
-                return true;
+            for(int j=0; j < size; j++)
+                if(aSet[i].equals(anotherSet.aSet[j]))
+                    count++;
         }
-        return false;
+            return count == size ? true : false;
+
+    }
+    public int size(){
+        return size;
     }
 
     public String toString(){
